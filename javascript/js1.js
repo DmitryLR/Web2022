c9dcc7ae582b505c5df8c72e59ab4b419270d573
DmitryLR <dekaromail@gmail.com>
2022-03-06 20:37:38 +0200

    New Project

---
+++ javascript/js1.js
@@ -0,0 +1,37 @@
+// - комментарий писать обязательно также ctrl + /
+
+// типы данных
+
+// число Integer или INT - Number
+//let x = 123;
+x = "kuku"
+// String STR - Text
+str = "Hello World"; 
+let str2 = ""
+// Float 
+let x2 = 2.45;
+// Boolean для построения логики
+let bool = true; //false тру  это 1 и фолс это 0 
+//Arrays массивы
+lat arr = ["bmw, 2.5,true"];
+//Objects
+let obj1 = {
+    name:"Vitaly",
+    age:37
+}
+let
+a=1,
+b=3, 
+c=4;
+let ar2;
+
+// document.write(x,str,bool); // вывод данных в документ
+// document.write(arr[1]);
+// document.write(obj1.age);
+//console.log(arr,obj1); //вывод данных в консоль
+
+//ES6 let для объявления переменных
+// const author ="v.k.";
+
+let answer = x * x2;
+document.write(answer);
